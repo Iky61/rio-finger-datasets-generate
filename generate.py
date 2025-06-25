983c4be69cc04e9636997f17b284f324fbcef580
Iky61 <dicky.gps105@gmail.com>
2025-06-25 15:48:23 +0800

    perbaiki konsep merge left

--- generate.py
+++ generate.py
@@ -22,10 +22,22 @@ class generator:
 
         # feature engineering
         db_emp['id_2'].replace('37#RIO Office Equipment','37#RIO Utama', inplace=True)
+
+        #  filter branch
+        if branch == 'RIO Digital Printing':
+            branch_target = ['RIO Digital Printing']
+        else:
+            branch_target = ['RIO Office Equipment', 'RIO Utama']
+
+        # filter database karyawan
+        db_emp = db_emp[db_emp.branch.isin(branch_target)]
         
         # merge data 
-        data = data.merge(db_emp[db_emp.status_bekerja == 'Aktif'][['id_2','nama_karyawan_(sesuai_ktp)']], on='id_2', how='left').drop(columns=['id_2','branch'])
-        data.columns = ['id','cek_in','cek_rest_out','cek_rest_in','cek_out','emp_name']
+        data = db_emp[db_emp.status_bekerja == 'Aktif'][['id_2','nama_karyawan_(sesuai_ktp)']].merge(data, on='id_2', how='left')
+        data['id'] = data['id_2'].apply(lambda x: str(x).split('#')[0])
+
+        data = data.drop(columns=['id_2','branch'])
+        data.columns = ['emp_name','id','cek_in','cek_rest_out','cek_rest_in','cek_out']
         data = data[['id','emp_name','cek_in','cek_rest_out','cek_rest_in','cek_out']]
         data = data.dropna(subset=['emp_name'])
 
